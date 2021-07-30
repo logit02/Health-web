4ae291faeca63d486447be02dfd88e67de9445b1
Inesa Toroyan <inesatoroyan@Inesas-MacBook-Pro.local>
2021-07-30 21:42:06 +0400

    health-website

---
+++ src/servies.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import './services.css';
+
+function Services(){
+    return(
+        <div className='all'>
+            <div className='puretext'>
+
+                    <h1>Our Servies</h1>
+                    <p>.</p>
+                    <p className='desc'>We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
+
+
+            </div>
+
+
+
+
+        </div>
+
+
+    )
+}
+
+
+export default Services;
